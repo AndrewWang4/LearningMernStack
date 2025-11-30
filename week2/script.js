a3b99a6afe6f8bada088ae06eb16e9ac9ed33416
AndrewWang4 <andrewhtwe@gmail.com>
2025-11-30 09:50:44 -0800

    Added upload message feature.

--- week2/script.js
+++ week2/script.js
@@ -1,6 +1,9 @@
 const button = document.getElementById("button");
 const voicemode = document.getElementById("voicemode");
 const voice = document.getElementById("voice");
+const input = document.getElementById("inputtext");
+const upload = document.getElementById("uploadmessage");
+const disableid = document.getElementById("disable");
 button.addEventListener("mouseover", () => {
     document.getElementById("hoverText").style.display = "block";
 });
@@ -19,3 +22,24 @@ voice.addEventListener("mouseover", () => {
 voice.addEventListener("mouseout", () => {
     document.getElementById("voicehover").style.display = "none";
 });
+input.addEventListener("input", value);
+/*
+document.getElementById("voicemode").style.display = "none";
+document.getElementById("voicehover").style.display = "none";
+document.getElementById("inputtext").style.display = "block";
+document.getElementById("disable").style.display = "block";
+*/
+function value(e) {
+    if (e.target.value.length === "") {
+        console.log("Found nothing.")
+        document.getElementById("voicemode").style.display = "block";
+        document.getElementById("uploadmessage").style.display = "none";
+        document.getElementById("disable").style.display = "none";
+    } else {
+        console.log("Text typed.")
+        document.getElementById("voicemode").style.display = "none";
+        document.getElementById("voicehover").style.display = "none";
+        document.getElementById("uploadmessage").style.display = "block";
+        document.getElementById("disable").style.display = "block";
+    }
+}
